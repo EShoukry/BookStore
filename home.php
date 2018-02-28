9061aabee544044aa9321051a772e32bee37b489
Ivan Rojas <iroja013@fiu.edu>
2018-02-28 17:04:32 -0500

    added navbar_user.php for when user navbar needed. updates occurences in home and edituser

--- home.php
+++ home.php
@@ -54,43 +54,9 @@ $userRow = mysqli_fetch_array($res, MYSQLI_BOTH);
             <img src="images/index.jpeg" alt="Team 7 book store" >
         </div>  
 
-	<div id="hd_container">
-	<div class="container">
-			<nav class="navbar navbar-light bg-faded" style="background-color: biege">
-			<div class="container-fluid">
-			<ul class="nav navbar-nav navbar-right">
-					<li class="nav-item dropdown nohover">
-						<a href="#" class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-							<span class="glyphicon glyphicon-th-list"></span>&nbsp;Hi <?php echo $userRow['u_login_id']; ?>&nbsp;<span class="caret"></span>
-						</a>
-						<div class="dropdown-menu dropdown-menu-right " aria-labelledby="navbarDropdownMenuLink" >
-
-						<!-- Placeholder for edit info and credit cards-->
-							<a class="dropdown-item" href="home.php">
-								<span class="glyphicon glyphicon-user"></span>&nbsp;View Profile
-							</a><br>
-							<a class="dropdown-item" href="edituser.php">
-								<span class="glyphicon glyphicon-edit"></span>&nbsp;Edit Profile
-							</a><br>
-							<a class="dropdown-item" href="#">
-								<span class="glyphicon glyphicon-credit-card"></span>&nbsp;Add/Edit Payment Methods
-							</a><br>
-							<a class="dropdown-item" href="#">
-								<span class="glyphicon glyphicon-envelope"></span>&nbsp;Add/Edit Addresses
-							</a>
-							
-							<!--Leave sign out for last option. -->
-							<hr />
-							<a class="nav-item dropdown-item" href="logout.php?logout">
-								<span class="glyphicon glyphicon-log-out"></span>&nbsp;Sign Out
-							</a><br>
-						</div>
-					</li>
-				</ul>
-				</div>
-			</nav>
-	</div>
-	</div>
+	<?php
+        require "includes/navbar_user.php";
+    ?>
 	
 	<div id="wrapper">
 	<div class="container">
---
+++ includes/navbar_user.php
@@ -0,0 +1,37 @@
+<div id="hd_container">
+	<div class="container">
+			<nav class="navbar navbar-light bg-faded" style="background-color: biege">
+			<div class="container-fluid">
+			<ul class="nav navbar-nav navbar-right">
+					<li class="nav-item dropdown nohover">
+						<a href="#" class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							<span class="glyphicon glyphicon-th-list"></span>&nbsp;Hi <?php echo $userRow['u_login_id']; ?>&nbsp;<span class="caret"></span>
+						</a>
+						<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink" >
+
+						<!-- Placeholder for edit info and credit cards-->
+							<a class="dropdown-item" href="home.php">
+								<span class="glyphicon glyphicon-user"></span>&nbsp;View Profile
+							</a><br>
+							<a class="dropdown-item " href="edituser.php">
+								<span class="glyphicon glyphicon-edit"></span>&nbsp;Edit Profile
+							</a><br>
+							<a class="dropdown-item" href="#">
+								<span class="glyphicon glyphicon-credit-card"></span>&nbsp;Add/Edit Payment Methods
+							</a><br>
+							<a class="dropdown-item " href="#">
+								<span class="glyphicon glyphicon-envelope"></span>&nbsp;Add/Edit Addresses
+							</a>
+							<hr />
+							<!--Leave sign out for last option. -->
+							<a class="dropdown-item " href="logout.php?logout">
+								<span class="glyphicon glyphicon-log-out"></span>&nbsp;Sign Out
+							</a><br>
+							
+						</div>
+					</li>
+				</ul>
+				</div>
+			</nav>
+	</div>
+	</div>
