b529543bddb828c284750aecddc4405aef2ff0df
Alejandro Vidal <alevidal2014@gmail.com>
2018-03-27 18:50:12 -0400

    Add features for the details.php

--- includes/books_shown.php
+++ includes/books_shown.php
@@ -11,20 +11,20 @@
                     
                                     
                     //Quary to obtain the authors base on the book id
-                    $result1 = $mysqli->query("SELECT a_name FROM authors, books_authors WHERE books_authors.book_id='".$row["book_id"]."' AND books_authors.author_id= authors.author_id");
+                    $result1 = $mysqli->query("SELECT a_name, books_authors.author_id FROM authors, books_authors WHERE books_authors.book_id='".$row["book_id"]."' AND books_authors.author_id= authors.author_id");
                     
                     if (!$result1) {
                         die('Invalid Query 1: ' . mysql_error());
                     }
                     
                     echo '<div class="book_container">';
-                    echo '<div class="book_cover"><img src="'.$row["b_picture"].'" class="cover_img"></div>';         
-                    echo '<div class="book_name">'.$row["b_name"].'</div>';
+                    echo '<div class="book_cover"><a href="details.php?b_id='.$row["book_id"].'"><img src="'.$row["b_picture"].'" class="cover_img"></div></a>';         
+                    echo '<div class="book_name"><a href="details.php?b_id='.$row["book_id"].'">'.$row["b_name"].'</a></div>';
                     echo '<div class="book_author"><span>author</span>';
                     
                     $temp_count = $result1->num_rows;
                     while($row1 = $result1->fetch_assoc()){
-                        echo $row1["a_name"]; 
+                        echo '<a href="details.php?a_id='.$row1["author_id"].'">'.$row1["a_name"].'</a>'; 
                         if($temp_count >1){
                             echo ", ";
                             $temp_count--;
